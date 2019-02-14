aa56a920dfeab7126eb529f87d6d8b1f6b20f8d2
Hery-Rs <hery.rasamimanana@gmail.com>
2019-02-14 18:50:23 +0100

    add form

---
+++ form.php
@@ -0,0 +1,17 @@
+<h1>Ajouter un contact</h1>
+<form id="create-form" action="" method="post">
+    <!-- Prénom -->
+    <div class="form-group">
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" class="form-control" id="prenom" placeholder="votre prénom">
+    </div>
+
+    <!-- Nom -->
+    <div class="form-group">
+        <label for="nom">Nom</label>
+        <input type="text" name="nom" class="form-control" id="nom" placeholder="votre nom">
+    </div>
+
+    <!-- Valider -->
+    <input type="submit" class="create btn btn-primary" value="Valider">
+</form>
